a1bac34178696e34e657f5fadaa04e7f27f11fcb
ttobaegi <ttobaegi0308@gmail.com>
2021-03-07 11:00:27 +0900

    check

--- 1_Programmers.sql
+++ 1_Programmers.sql
@@ -1,4 +1,4 @@
-## MIN MAX SUM 
+## MIN MAX SUM COUNT
 -- ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다. 
 -- ANIMAL_INS 테이블 구조는 다음과 같으며, ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
 NAME	TYPE	NULLABLE
@@ -83,6 +83,7 @@ FROM ANIMAL_OUTS
 WHERE @HOUR <23										-- @HOUR이<23까지 >> (@HOUR+1 이 23이 될 때까지) @HOUR값이 +1씩 증가
 
 
+## IS NULL
 -- Q8. 동물 보호소에 들어온 동물 중, 이름이 없는 채로 들어온 동물의 ID를 조회하는 SQL 문을 작성해주세요. 단, ID는 오름차순 정렬되어야 합니다.
 SELECT ANIMAL_ID
 FROM ANIMAL_INS
@@ -103,6 +104,8 @@ SELECT ANIMAL_TYPE
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID
 
+
+## JOIN
 -- Q11.천재지변으로 인해 일부 데이터가 유실되었습니다. 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL문을 작성해주세요.
 SELECT O.ANIMAL_ID
 	, O.NAME
